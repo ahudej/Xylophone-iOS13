5dca27ef5cc988ecb75e8af1aaf4a417852e31fb
Angelo <angelo.hudej@gmx.at>
2021-02-09 10:48:29 +0100

    Fertiges Projekt  Beim Drücken kommt der Ton und es färbt sich für 2ms.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
+    @IBOutlet weak var BtnC: UIButton!
+    @IBOutlet weak var BtnD: UIButton!
+    @IBOutlet weak var BtnE: UIButton!
+    @IBOutlet weak var BtnF: UIButton!
+    @IBOutlet weak var BtnG: UIButton!
+    @IBOutlet weak var BtnA: UIButton!
+    @IBOutlet weak var BtnB: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func BtnCPressed(_ sender: UIButton) {
+        playSound(tone: sender.currentTitle,btn: sender)
+    }
     
+    func playSound(tone: String?, btn: UIButton) {
+        
+        btn.alpha = 0.5
+        
+        let url = Bundle.main.url(forResource: tone, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+        usleep(200000)
+        btn.alpha = 1
+
+    }
 
 }
+
+
+
 
